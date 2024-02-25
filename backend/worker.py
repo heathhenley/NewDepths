44e54398998997fc1c0b88d92b7be7b46a1898ce
Heath Henley <heath.j.henley@gmail.com>
2024-02-25 11:54:05 -0500

    Fix sort, add way to get data, email notification

--- backend/worker.py
+++ backend/worker.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import logging
 import os
 import requests
+import urllib.parse
 
 import resend
 from sqlalchemy.orm import Session
@@ -14,6 +15,11 @@ from db import models
 
 load_dotenv()
 resend.api_key = os.getenv("RESEND_KEY")
+
+
+def build_url(base_url: str, query_params: dict) -> str:
+  """ Build a url from a base url and a dictionary of query parameters. """
+  return f"{base_url}?{urllib.parse.urlencode(query_params)}"
 
 
 def bbox_to_envelope(bbox: models.BoundingBox) -> str:
@@ -41,7 +47,7 @@ def multibeam_query_params(
     'spatialRel': 'esriSpatialRelIntersects',
     'outFields': 'SURVEY_ID,PLATFORM,DOWNLOAD_URL,START_TIME,END_TIME,ENTERED_DATE',
     'returnGeometry': False,
-    'orderByFields': 'ENTERED_DATE'
+    'orderByFields': 'ENTERED_DATE DESC'
   }
 
 
@@ -57,7 +63,7 @@ def csb_query_params(
     'spatialRel': 'esriSpatialRelIntersects',
     'outFields': 'NAME,PLATFORM,ARRIVAL_DATE,START_DATE,YEAR',
     'returnGeometry': False,
-    'orderByFields': 'ARRIVAL_DATE'
+    'orderByFields': 'ARRIVAL_DATE DESC'
   }
 
 
@@ -88,19 +94,26 @@ def get_data_for_bbox(
     if res.status_code != 200:
       raise Exception(f"Bad response from NOAA: {res.status_code}")
   except Exception as e:
-    print(f"Error getting data for bbox {bbox.id}: {e}")
+    logging.error(f"Error getting data for bbox {bbox.id}: {e}")
     return None
   return res.json()
+
 
 # Make this a template?
 def make_email_body(notifications):
   """ Make the email body for the user. """
+  type_map = {
+    "multibeam": "Multibeam",
+    "csb0": "CSB Line Data",
+    "csb1": "CSB Point Data",
+  }
   body = "<h1> New data found for your bounding boxes! </h1>"
   for notification in notifications:
     bbox = notification["bbox_id"]
-    data_type = notification["data_type"]
+    data_type = type_map[notification["data_type"].name]
+
     new_surveys = notification["new_surveys"]
-    body += f"<h2> New '{data_type.name}' data for bbox #{bbox.id} </h2>"
+    body += f"<h2> New '{data_type}' data for bbox</h2>"
     body += (
       f"<h3>BBOX: ({bbox.top_left_lat:.2f}, {bbox.top_left_lon:.2f}), "
       f"({bbox.bottom_right_lat:.2f}, {bbox.bottom_right_lon:.2f})</h3>"
@@ -108,16 +121,18 @@ def make_email_body(notifications):
     body += f"<p>There are {len(new_surveys)} new surveys for this box.</p>"
     body += "<ul>"
     for survey in new_surveys[:5]:
-      sid = survey["attributes"].get(
-        "SURVEY_ID", survey["attributes"].get("NAME", "Unknown"))
+      t = survey["attributes"].get(
+        "ENTERED_DATE", survey["attributes"].get("ARRIVAL_DATE"))
+      body += f"<li>{datetime.fromtimestamp(t / 1000.0)}"
       pf = survey["attributes"]["PLATFORM"]
       dl = survey["attributes"].get("DOWNLOAD_URL", "")
-      body += f"<li>ID: {sid}, platform: {pf}"
+      body += f", Platform: {pf}"
       if dl:
         body += f", <a href={dl}>Link</a></li>"
     body += "</ul>"
     if len(new_surveys) > 5:
       body += f"<p>And {len(new_surveys) - 5} more...</p>"
+    body += f'<a href={notification["url"]}> JSON </a>'
   return body
 
 
@@ -131,11 +146,9 @@ def check_for_new_data(
   for bbox in bboxes:
     for data_type in data_types:
 
-      print(f"Getting updates for: {data_type.name}")
-
       # get the last cached date for this bbox
       date = crud.get_last_cached_date(db, bbox, data_type)
-      print(f"Update bbox {bbox.id}, "
+      logging.info(f"Update bbox {bbox.id}, "
             f"for data type {data_type.name}, "
             f"last data from: {date}")
 
@@ -143,33 +156,30 @@ def check_for_new_data(
 
       url = data_type.base_url
       if not (new_data := get_data_for_bbox(url, query_params, bbox)):
-        print(f"No new data for bbox {bbox.id}")
+        logging.info(f"No new data for bbox {bbox.id}")
         continue
 
       if "error" in new_data:
-        print(
+        logging.error(
           f"Error in {data_type.name} "
           f"bb:{bbox.id}: {new_data['error']}")
         continue
       
       if not (surveys := new_data['features']):
-        print(f"No new data for bbox {bbox.id}")
+        logging.info(f"No new data for bbox {bbox.id}")
         continue
 
       time_key = "ENTERED_DATE" if data_type.name == "multibeam" else "ARRIVAL_DATE"
       # get the latest date from the new data, this will be cached 
-      latest_date = surveys[-1]["attributes"][time_key] # it's sorted
+      latest_date = surveys[0]["attributes"][time_key] # it's sorted
       latest_datetime = datetime.fromtimestamp(latest_date / 1000.0)
-      print(f"Latest date for bbox {bbox.id}: {latest_datetime}")
+      logging.info(f"Latest date for bbox {bbox.id}: {latest_datetime}")
 
       if date is not None:
         surveys = [
           s for s in surveys
           if s["attributes"][time_key] / 1000.0 > date.timestamp()
         ]
-
-      print(f"BBOX: {bbox.id} has {len(surveys)} "
-            f"new {data_type.name} surveys since {date}.")
 
       # upsert in the database
       crud.set_last_cached_date(db, bbox, data_type, latest_datetime)
@@ -179,7 +189,8 @@ def check_for_new_data(
           {
             "bbox_id": bbox,
             "data_type": data_type,
-            "new_surveys": surveys
+            "new_surveys": surveys,
+            "url": build_url(url, query_params)
           }
         )
   return notifications_by_user
