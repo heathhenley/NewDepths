b7c972c02d60a630d5a1e8f9e71e1ddb81423d43
Heath Henley <heath.j.henley@gmail.com>
2024-02-29 22:51:58 -0500

    try pool recycle param

--- backend/db/database.py
+++ backend/db/database.py
@@ -8,7 +8,7 @@ import os
 load_dotenv()
 SQLALCHEMY_DATABASE_URL = os.getenv("DB_CONNECT_STR")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_recycle=3600)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
