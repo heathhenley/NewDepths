97811b5e4bcc34d3f5360db1578c10f097ecd149
Heath Henley <heath.j.henley@gmail.com>
2024-11-05 23:44:54 -0500

    move db string to settings

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,14 +1,12 @@
+import os
+
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-import os
+from settings import SQLALCHEMY_DATABASE_URL
 
-load_dotenv()
-SQLALCHEMY_DATABASE_URL = os.getenv(
-  "DB_CONNECT_STR",
-  "postgresql://postgres:postgres@localhost:5432/test_db")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_recycle=3600)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- backend/settings.py
+++ backend/settings.py
@@ -3,10 +3,14 @@ import os
 from dotenv import load_dotenv
 
 
-load_dotenv()
+load_dotenv(os.getenv("ENV_FILE", ".env"))
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+SQLALCHEMY_DATABASE_URL = os.getenv(
+  "DB_CONNECT_STR",
+  "postgresql://postgres:postgres@localhost:5432/test_db")
+
 
 if SECRET_KEY is None or ALGORITHM is None:
   raise Exception("SECRET_KEY and ALGORITHM must be set in .env file")
